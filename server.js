d76a65da5549cd47f02fa21700c1c1d65e07b9b6
Brian Kiplagat <kiplagatbrian18@gmail.com>
2025-02-12 21:20:49 +0300

    fix

--- server.js
+++ server.js
@@ -96,7 +96,10 @@ app.post("/add-border", async (req, res) => {
         .json({ error: "border_size must be between 0 and 100" });
     }
 
-    const base64Data = originalBase64Data.replace(/^data:image\/\w+;base64,/, "");
+    const base64Data = originalbase64Image.replace(
+      /^data:image\/\w+;base64,/,
+      ""
+    );
     const imageBuffer = Buffer.from(base64Data, "base64");
 
     let processedImageBuffer;
